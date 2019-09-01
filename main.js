1477a520268d1a5c6f1b23f75cbdd02683acbfd3
Weslley Victor <wvictor07@gmail.com>
2019-09-01 17:27:02 +0100

    Commit before major changes using nodejs, express and socket.io

--- main.js
+++ main.js
@@ -1,67 +1,78 @@
-var input = document.getElementById('input');
-var word = document.querySelector('.word');
-var text = document.getElementById('text');
-var timer = document.querySelector('.timer');
-var btnStart = document.querySelector('.btnStart');
-let progress = document.querySelector('.progress');
-let progressInput = document.querySelector('.progress-input');
+const input = document.getElementById('input')
+const word = document.querySelector('.word')
+const text = document.getElementById('text')
+const timer = document.querySelector('.timer')
+const btnStart = document.querySelector('.btnStart')
+const btnStop = document.querySelector('.btnStop')
+const progress = document.querySelector('.progress')
+const progressInput = document.querySelector('.progress-input')
+const author = document.querySelector('.author')
 
 
 
-var count = 1; //Changed to upward timer.
-var c = 0
+let count = 60 //Changed to a decreasing timer.
+let c = 0
 //var firstLevel = ['porta', 'cadeira', 'palavra', "era uma casa muito engraçada", 12345, "era uma vez", "um lugar tão, tão distante", "eu consigo voar", "cada macaco no seu galho", "filho de peixe, peixinho é"];
-var level;
-/*API CALL*/
-let quotes = [];
-let authors = [];
+
+/*
+*API CALL
+*/
+
+let quotes = []
+let authors = []
 
 function getQuotes(){
-	let xhr = new XMLHttpRequest();
-	let token = 'a0e73df66fca35709b1301fb215bb2ec';
-	xhr.open('GET', 'https://favqs.com/api/quotes/');
-	xhr.setRequestHeader("Authorization", 'Token token="' + token+'"');
+	let xhr = new XMLHttpRequest()
+	let token = 'a0e73df66fca35709b1301fb215bb2ec'
+	xhr.open('GET', 'https://favqs.com/api/quotes/?page=2')
+	xhr.setRequestHeader("Authorization", 'Token token="' + token+'"')
 
 	xhr.onload = function () {
 	    // Process our return data
 	    if (xhr.status >= 200 && xhr.status < 300) {
 	        // What do when the request is successful
-	        var json = this.responseText;
-	        var data = JSON.parse(this.responseText);
+	        var json = this.responseText
+	        var data = JSON.parse(this.responseText)
 	        //console.log(data);
-
 	        for(let i=0;i<25;i++){
-	        	if(data.quotes[i].body.length < 60){
-	        		quotes.push(data.quotes[i].body);
+	        	if(data.quotes[i].body.length < 60){//max characters the quote will have
+	        		quotes.push(data.quotes[i].body)
+							authors.push(data.quotes[i].author)
 	            	//authors.push(data.quotes[i].author);
 	        	}
 	        }
-	        word.innerHTML = quotes[c];
-	        console.log(data);
-
-	        
-	        
-
+	        word.innerHTML = quotes[c]
+					author.innerHTML = authors[c]
+	        console.log(data)
 	    } else {
 	        // What do when the request fails
-	        console.log('The request failed!');
+	        console.log('The request failed!')
 	    }
 	};
-	xhr.send();
-
-
+	xhr.send()
 }
 
-
-
 btnStart.onclick = function(){
-	var t = counter(count)
-	
-	input.style.display = 'block';
-	word.style.display = 'block';
-	timer.style.display = 'block';
+	//var t = counter(count)
+	input.style.display = 'block'
+	word.style.display = 'block'
+	//timer.style.display = 'block'
 	getQuotes();
-	
+}
+//btnStop.addEventListener('click', stop)
+
+function stop(){
+	//clearTimeout(time)
+	word.innerHTML = ''
+	author.innerHTML = ''
+	timer.innerHTML = ''
+	input.style.display = 'none'
+	word.style.display = 'none'
+	timer.style.display = 'none'
+	progress.style.width = "0%"
+	progressInput.style.width ="0%"
+	c = 0
+	quotes=[] //erase all data in the variable quotes.
 }
 
 
@@ -70,81 +81,68 @@ let pos = 1;
 input.onkeyup = function(){
 
 	/*Progress while typing*/
-	let lengthInput = input.value.length; //length of input text
-	let lengthQuote = quotes[c].length; //length of the quote the user need to write
-	let stringQuote = quotes[c].substring(0, lengthInput);
+	let lengthInput = input.value.length //length of input text
+	let lengthQuote = quotes[c].length //length of the quote the user need to write
+	let stringQuote = quotes[c].substring(0, lengthInput)
 
 	//console.log(input.value, stringQuote);
 
 	if(input.value == stringQuote){
 		pos++;
 		//console.log(lengthInput/lengthQuote)
-		progressInput.style.backgroundColor = 'green';
+		progressInput.style.backgroundColor = 'green'
 		progressInput.style.width = (lengthInput/lengthQuote)*100+"%";
-		
 	}
 	else{
-		progressInput.style.backgroundColor = 'red';
+		progressInput.style.backgroundColor = 'red'
 	}
 	if(input.value==''){
-		pos = 1;
+		pos = 1
 	}
-
-
 	//When phrase is typed correctally
 	if(input.value == word.innerHTML){
-		input.value = '';
+		input.value = ''
 		c++;
-		fraction = (c/quotes.length)*100;
-		progress.style.width = fraction+"%";
-		progressInput.style.width = "0%";
-		clearTimeout(time);
-		word.innerHTML = quotes[c];
-		counter(count);
-
-		
+		fraction = (c/quotes.length)*100
+		progress.style.width = fraction+"%"
+		progressInput.style.width = "0%"
+		word.innerHTML = quotes[c]
+		author.innerHTML = authors[c]
 
 		if (c == quotes.length){
-			word.innerHTML = '';
-			timer.innerHTML = '';
-			input.style.display = 'none';
-			word.style.display = 'none';
-			timer.style.display = 'none';
-			progress.style.width = "0%";
+			console.log(timer.innerHTML)
+			word.innerHTML = ''
+			author.innerHTML = ''
+			timer.innerHTML = ''
+			input.style.display = 'none'
+			word.style.display = 'none'
+			timer.style.display = 'none'
+			progress.style.width = "0%"
 			progressInput.style.width ="0%"
-			c = 0;
-			clearTimeout(time);
-			quotes=[]; //erase all data in the variable quotes.
-
-			//input.style.display = 'none';
-			//word.style.display = 'none';
-			//clearTimeout(time);
-			
+			c = 0
+			//clearTimeout(time)
+			quotes=[] //erase all data in the variable quotes.
 		}
-		
 	}
-	
 }
 
-var time; //This allows me to stop the setTimeout function!
 function counter(x){
 	if(x >= 0){
-		timer.innerHTML = x + " s";
-		time = setTimeout(function(){counter(x)}, 1000);
+		timer.innerHTML = x + " s"
+		time = setTimeout(function(){counter(x)}, 1000)
 
 		if(x == 0){
-			alert('Your time is up!');
-			word.innerHTML = '';
-			input.value = '';
+			alert('Your time is up!')
+			word.innerHTML = ''
+			author.innerHTML = ''
+			input.value = ''
 			c = 0;
-			input.style.display = 'none';
-			word.style.display = 'none';
-			timer.style.display = 'none';
+			input.style.display = 'none'
+			word.style.display = 'none'
+			timer.style.display = 'none'
+			progress.style.width = "0%"
+			progressInput.style.width ="0%"
 		}
-		x++;//Changed to upward timer.
-
+		x--//Changed to a decreasing timer.
 	}
-	
-	
-	
 }
