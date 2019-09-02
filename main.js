34261a24c650df704ca07d6ede4933ddb27f2d51
Weslley Victor <wvictor07@gmail.com>
2019-09-02 01:04:06 +0100

    Implemented a rough version of the multiplayer game
    1)Implemented the system of progress for users to see.
    2)Implemented multiplayer for up to 3 users (as far as I tested). Now users can play multiplayer against each other!!!
    3)Made so that users cannot copy the text of the page (to cheat and finish faster)
    
    I used the following code to achieve that:
    	-webkit-touch-callout: none;
    	-webkit-user-select: none;
    	-khtml-user-select: none;
    	-moz-user-select: none;
    	-ms-user-select: none;
    	user-select: none;
    
    I spend the whole day having a lot of problems. One I was having was about the server storing two version of the quotes and authors. I solved that by making sure only one user triggers one event that takes the data to the server. Otherwise it won't work.

--- main.js
+++ main.js
@@ -1,148 +1,62 @@
-const input = document.getElementById('input')
-const word = document.querySelector('.word')
-const text = document.getElementById('text')
-const timer = document.querySelector('.timer')
-const btnStart = document.querySelector('.btnStart')
-const btnStop = document.querySelector('.btnStop')
-const progress = document.querySelector('.progress')
-const progressInput = document.querySelector('.progress-input')
-const author = document.querySelector('.author')
+const express = require('express')
+const app = express()
+const http = require('http').createServer(app)
+const io = require('socket.io')(http)
 
 
+function deleteUser(users, deleteUser){//function to delete user from users array
+  for (let i=0;i<users.length;i++){
+    if(users[i] == deleteUser){
+        users.splice(i,1)
+    }
+  }
+  return users
+}
 
-let count = 60 //Changed to a decreasing timer.
-let c = 0
-//var firstLevel = ['porta', 'cadeira', 'palavra', "era uma casa muito engraçada", 12345, "era uma vez", "um lugar tão, tão distante", "eu consigo voar", "cada macaco no seu galho", "filho de peixe, peixinho é"];
+app.use(express.static('static'))
 
-/*
-*API CALL
-*/
+app.get('/', (req, res)=>{
+  res.sendFile(__dirname+'/index.html')
+})
 
+let users = []
 let quotes = []
 let authors = []
 
-function getQuotes(){
-	let xhr = new XMLHttpRequest()
-	let token = 'a0e73df66fca35709b1301fb215bb2ec'
-	xhr.open('GET', 'https://favqs.com/api/quotes/?page=2')
-	xhr.setRequestHeader("Authorization", 'Token token="' + token+'"')
+io.on('connection', (socket)=>{
 
-	xhr.onload = function () {
-	    // Process our return data
-	    if (xhr.status >= 200 && xhr.status < 300) {
-	        // What do when the request is successful
-	        var json = this.responseText
-	        var data = JSON.parse(this.responseText)
-	        //console.log(data);
-	        for(let i=0;i<25;i++){
-	        	if(data.quotes[i].body.length < 60){//max characters the quote will have
-	        		quotes.push(data.quotes[i].body)
-							authors.push(data.quotes[i].author)
-	            	//authors.push(data.quotes[i].author);
-	        	}
-	        }
-	        word.innerHTML = quotes[c]
-					author.innerHTML = authors[c]
-	        console.log(data)
-	    } else {
-	        // What do when the request fails
-	        console.log('The request failed!')
-	    }
-	};
-	xhr.send()
-}
+  socket.on('joined', (username)=>{
+    users.push(username)
+    socket.username = username
+    io.emit('joined', users)
+    console.log(users)
+  })
+  socket.on('start', ()=>{
+    io.emit('start')
+  })
+  socket.on('get quotes', (clientQuotes)=>{
+    quotes = clientQuotes.quotes
+    authors = clientQuotes.authors
+    console.log(quotes)
+    io.emit('get quotes', {quotes: quotes, authors: authors})
 
-btnStart.onclick = function(){
-	//var t = counter(count)
-	input.style.display = 'block'
-	word.style.display = 'block'
-	//timer.style.display = 'block'
-	getQuotes();
-}
-//btnStop.addEventListener('click', stop)
+  })
 
-function stop(){
-	//clearTimeout(time)
-	word.innerHTML = ''
-	author.innerHTML = ''
-	timer.innerHTML = ''
-	input.style.display = 'none'
-	word.style.display = 'none'
-	timer.style.display = 'none'
-	progress.style.width = "0%"
-	progressInput.style.width ="0%"
-	c = 0
-	quotes=[] //erase all data in the variable quotes.
-}
+  socket.on('user progress', (progress)=>{
+    io.emit('user progress', {progress: progress*100,username: socket.username})
+  })
 
+    socket.on('disconnect', ()=>{
+      io.emit('left', socket.username)
+      users = deleteUser(users, socket.username)
+      console.log(users)
+    })
 
+    socket.on('game over',()=>{
+      io.emit('game over', socket.username)
+    })
+})
 
-let pos = 1;
-input.onkeyup = function(){
-
-	/*Progress while typing*/
-	let lengthInput = input.value.length //length of input text
-	let lengthQuote = quotes[c].length //length of the quote the user need to write
-	let stringQuote = quotes[c].substring(0, lengthInput)
-
-	//console.log(input.value, stringQuote);
-
-	if(input.value == stringQuote){
-		pos++;
-		//console.log(lengthInput/lengthQuote)
-		progressInput.style.backgroundColor = 'green'
-		progressInput.style.width = (lengthInput/lengthQuote)*100+"%";
-	}
-	else{
-		progressInput.style.backgroundColor = 'red'
-	}
-	if(input.value==''){
-		pos = 1
-	}
-	//When phrase is typed correctally
-	if(input.value == word.innerHTML){
-		input.value = ''
-		c++;
-		fraction = (c/quotes.length)*100
-		progress.style.width = fraction+"%"
-		progressInput.style.width = "0%"
-		word.innerHTML = quotes[c]
-		author.innerHTML = authors[c]
-
-		if (c == quotes.length){
-			console.log(timer.innerHTML)
-			word.innerHTML = ''
-			author.innerHTML = ''
-			timer.innerHTML = ''
-			input.style.display = 'none'
-			word.style.display = 'none'
-			timer.style.display = 'none'
-			progress.style.width = "0%"
-			progressInput.style.width ="0%"
-			c = 0
-			//clearTimeout(time)
-			quotes=[] //erase all data in the variable quotes.
-		}
-	}
-}
-
-function counter(x){
-	if(x >= 0){
-		timer.innerHTML = x + " s"
-		time = setTimeout(function(){counter(x)}, 1000)
-
-		if(x == 0){
-			alert('Your time is up!')
-			word.innerHTML = ''
-			author.innerHTML = ''
-			input.value = ''
-			c = 0;
-			input.style.display = 'none'
-			word.style.display = 'none'
-			timer.style.display = 'none'
-			progress.style.width = "0%"
-			progressInput.style.width ="0%"
-		}
-		x--//Changed to a decreasing timer.
-	}
-}
+http.listen(3000, ()=>{
+  console.log("Server started at port 3000!")
+})
---
+++ static/js/main.js
@@ -0,0 +1,204 @@
+const socket = io()
+let count = 60 //Changed to a decreasing timer.
+let c = 0
+//var firstLevel = ['porta', 'cadeira', 'palavra', "era uma casa muito engraçada", 12345, "era uma vez", "um lugar tão, tão distante", "eu consigo voar", "cada macaco no seu galho", "filho de peixe, peixinho é"];
+
+
+function getTotalCharOfQuotes(arr){
+	num = 0
+	for(let i=0;i<arr.length;i++){
+		for(let j=0;j<arr[i].length;j++){
+			num++
+		}
+	}
+	return num
+}
+function getQuotes(){
+	/*
+	*API CALL
+	*/
+	let quotes = []
+	let authors = []
+
+	let xhr = new XMLHttpRequest()
+	let token = 'a0e73df66fca35709b1301fb215bb2ec'
+	xhr.open('GET', 'https://favqs.com/api/quotes/?page=2')
+	xhr.setRequestHeader("Authorization", 'Token token="' + token+'"')
+
+	xhr.onload = function () {
+			// Process our return data
+			if (xhr.status >= 200 && xhr.status < 300) {
+					// What do when the request is successful
+					var json = this.responseText
+					var data = JSON.parse(this.responseText)
+					//console.log(data);
+					for(let i=0;i<5;i++){
+						if(data.quotes[i].body.length < 100){//max characters the quote will have
+							quotes.push(data.quotes[i].body)
+							authors.push(data.quotes[i].author)
+						}
+					}
+					socket.emit('get quotes', {quotes: quotes, authors: authors})
+			}
+		 else {
+					// What do when the request fails
+					console.log('The request failed!')
+			}
+	}
+	xhr.send()
+
+}
+
+
+
+$('.btnStart').click(function(){//when clicked
+	getQuotes()
+	socket.emit('start')
+
+})
+
+//INTERACTION WITH SERVER
+//Starting game
+socket.on('start', ()=>{
+
+	$('.input').focus()
+	//var t = counter(count)
+	$('.input').css('display', 'block')
+	$('.word').css('display', 'block')
+	$('.author').css('display', 'block')
+	$('.btnStart').css('display', 'none')
+	$('.btnStop').css('display','none')
+	//timer.style.display = 'block'
+})
+
+socket.on('get quotes', (quotesServer)=>{
+
+	let quotes = quotesServer.quotes
+	let authors = quotesServer.authors
+
+	$('.word').html(quotes[c])
+	$('.author').html(authors[c])
+
+	let overallProgress = 0
+	let inputArray = [0]
+	$('.input').keyup(function(){//When user is typing
+		/*Progress while typing*/
+		console.log('test')
+		let lengthInput = $('.input').val().length//length of input text
+		let lengthQuote = quotes[c].length //length of the quote the user need to write
+		let stringQuote = quotes[c].substring(0, lengthInput)
+		let progressQuote = lengthInput/lengthQuote
+		let totalCharacters = getTotalCharOfQuotes(quotes)
+
+		//When phrase is typed correctally
+		if($('.input').val() == $('.word').html()){
+			inputArray.push(lengthInput) //gets the last value of the lengh input and saves it into the array. The total number of correct characters typed will be the sum of each round of length inputs.
+			$('.input').val('')
+			c++;
+			overallProgress = c/quotes.length
+			let fraction = overallProgress*100
+			$('.progress').css('width', fraction+"%")
+			$('.progress-input').css('width', '0%')
+			$('.word').html(quotes[c])
+			$('.author').html(authors[c])
+
+			if (c == quotes.length){
+				socket.emit('game over')
+				$('.word').html('')
+				$('.author').html('')
+				$('.timer').html('')
+				$('.input').css('display', 'none')
+				$('.word').css('display', 'none')
+				$('.timer').css('display', 'none')
+				$('.progress').css('width', '0%')
+				$('.progress-input').css('width', '0%')
+				$('.btnStart').css('display', 'block')
+				$('.btnStop').css('display','block')
+				c = 0
+				overallProgress = 0
+				//clearTimeout(time)
+				quotes=[] //erase all data in the variable quotes.
+			}
+		}
+		if($('.input').val() == stringQuote){//If user input is equal to quote input at that position (or, in other words, if user got the letter right)
+			$('.progress-input').css('backgroundColor', 'green')
+			$('.progress-input').css('width', (progressQuote*100)+"%")
+
+			correctInputs = lengthInput+inputArray.reduce((a,b)=>a+b) //number of correct inputs. correctInputs/totalCharacters gives you the porcentage of completion of the quotes.
+
+			socket.emit('user progress', (correctInputs/totalCharacters))
+		}
+		else{
+			$('.progress-input').css('backgroundColor', 'red')
+		}
+	})
+})
+
+
+
+$('.setUsername').submit((e)=>{//sending username to server (will be stored in a session)
+  e.preventDefault
+  socket.emit('joined', $('.user').val())
+  $('.username').css('display','none')
+  $('.main').css('display','block')
+  return false
+
+})
+
+socket.on('joined', (usernames)=>{
+	tag = ''
+  for(username of usernames){tag += '<span>'+username+'</span><div class="user-item '+username+'"></div>'}
+	$('.dashboard').html(tag)
+})
+socket.on('user progress', (data)=>{
+	$('.'+data.username+'').css('width', data.progress+'%')
+
+})
+socket.on('game over', (winner)=>{
+	$('.main').text(winner+' won the game!!!')
+
+})
+
+socket.on('left', (username)=>{
+	$('.'+username+'').remove()
+})
+
+
+
+
+
+//Timer functions - to use later
+function stop(){
+	//clearTimeout(time)
+	$('.word').html('')
+	$('.author').html('')
+	$('.timer').html('')
+	$('.input').css('display', 'none')
+	$('.word').css('display', 'none')
+	$('.timer').css('display', 'none')
+	$('.progress').css('width', '0%')
+	$('progress-input').css('widt', "0%")
+	c = 0
+	quotes=[] //erase all data in the variable quotes.
+}
+
+function counter(x){
+	if(x >= 0){
+		$('.timer').html(x + " s")
+		time = setTimeout(function(){counter(x)}, 1000)
+
+		if(x == 0){
+			alert('Your time is up!')
+			$('.word').html('')
+			$('.author').html('')
+			$('.input').val('')
+			c = 0;
+			$('.input').css('display', 'none')
+			$('.word').css('display', 'none')
+			$('.timer').css('display', 'none')
+			$('.progress').css('width', '0%')
+			$('.progress-input').css('width', '0%')
+		}
+		x--//Changed to a decreasing timer.
+	}
+}
